d90f477d52025ab144e47870b5cab69164c7d6dd
Daniel Ho <webdevhod@gmail.com>
2023-10-22 18:12:23 -0700

    Added Movie interface.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 import { TmdbService } from '../services/tmdb.service';
 import { lastValueFrom } from 'rxjs';
+import { Movie } from '../interfaces/movie.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  movieData: unknown;
-  title: string = 'Guns of the Navarone';
+  movieData: Movie | undefined;
+  url: string = '';
 
-  constructor(private tmdbService: TmdbService) { }
+  constructor(private tmdbService: TmdbService) {}
 
   async ngOnInit() {
-    this.movieData = await lastValueFrom(this.tmdbService.getMovieData());
-  }
-
-  onH1Click() {
-    alert('h1');
+    this.movieData = await lastValueFrom(this.tmdbService.getMovie());
+    this.url = `url('https://image.tmdb.org/t/p/w500${this.movieData.poster_path}') no-repeat`;
   }
 }
--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,25 +1,116 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Movie } from '../interfaces/movie.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TmdbService {
+  isTestingMode: boolean = true;
+  url: string = '';
+
   constructor(private http: HttpClient) {}
 
-  getMovies(): unknown {
-    return [
-      'The Shawshank Redemption',
-      'The Godfather',
-      'The Dark Knight',
-      'The Godfather: Part II',
-      'The Lord of the Rings: The Return of the King',
-    ];
+  getMovie(movieId: string = '343611'): Observable<Movie> {
+    if (!this.isTestingMode) {
+      // https://developer.themoviedb.org/reference/movie-details
+      const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=92aaa3fd6b1887e47a42036d7f615027`;
+      return this.http.get(url) as Observable<Movie>;
+    } else {
+      this.url = "url('https://image.tmdb.org/t/p/w500//qtafXiYDUMKxzsssU41qWey5oiT.jpg') no-repeat";
+      return of({
+        adult: false,
+        backdrop_path: '/ww1eIoywghjoMzRLRIcbJLuKnJH.jpg',
+        belongs_to_collection: {
+          id: 403374,
+          name: 'Jack Reacher Collection',
+          poster_path: '/qtafXiYDUMKxzsssU41qWey5oiT.jpg',
+          backdrop_path: '/vViRXFnSyGJ2fzMbcc5sqTKswcd.jpg',
+        },
+        budget: 60000000,
+        genres: [
+          {
+            id: 28,
+            name: 'Action',
+          },
+          {
+            id: 53,
+            name: 'Thriller',
+          },
+        ],
+        homepage: '',
+        id: 343611,
+        imdb_id: 'tt3393786',
+        original_language: 'en',
+        original_title: 'Jack Reacher: Never Go Back',
+        overview:
+          'When Major Susan Turner is arrested for treason, ex-investigator Jack Reacher undertakes the challenging task to prove her innocence and ends up exposing a shocking conspiracy.',
+        popularity: 51.706,
+        poster_path: '/cOg3UT2NYWHZxp41vpxAnVCOC4M.jpg',
+        production_companies: [
+          {
+            id: 82819,
+            logo_path: '/gXfFl9pRPaoaq14jybEn1pHeldr.png',
+            name: 'Skydance',
+            origin_country: 'US',
+          },
+          {
+            id: 83645,
+            logo_path: null,
+            name: 'Huahua Media',
+            origin_country: 'CN',
+          },
+          {
+            id: 3407,
+            logo_path: '/iVMjKOFyRvm9PW45lW1wW6TSvnj.png',
+            name: 'Shanghai Film Group',
+            origin_country: 'CN',
+          },
+          {
+            id: 21777,
+            logo_path: null,
+            name: 'TC Productions',
+            origin_country: 'US',
+          },
+          {
+            id: 4,
+            logo_path: '/gz66EfNoYPqHTYI4q9UEN4CbHRc.png',
+            name: 'Paramount',
+            origin_country: 'US',
+          },
+        ],
+        production_countries: [
+          {
+            iso_3166_1: 'CN',
+            name: 'China',
+          },
+          {
+            iso_3166_1: 'US',
+            name: 'United States of America',
+          },
+        ],
+        release_date: '2016-10-19',
+        revenue: 162146076,
+        runtime: 118,
+        spoken_languages: [
+          {
+            english_name: 'English',
+            iso_639_1: 'en',
+            name: 'English',
+          },
+        ],
+        status: 'Released',
+        tagline: 'Justice is Coming.',
+        title: 'Jack Reacher: Never Go Back',
+        video: false,
+        vote_average: 5.95,
+        vote_count: 4511,
+      } as Movie);
+    }
   }
 
-  getMovieData(): Observable<unknown> {
-    const url = `https://api.themoviedb.org/3/movie/343611?api_key=<api-key>`;
-    return this.http.get(url);
-  }
+  // getPerson
+
+  // getSearchResults
 }
